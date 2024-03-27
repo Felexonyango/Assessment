3447c5679bb422829da21c7599a0fc53d09e4615
Felexonyango <felexonyango02@gmail.com>
2024-03-27 11:51:21 +0300

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -34,28 +34,27 @@ function App() {
       <div className=" container">
       <h5 className="mt-2 text-lg font-medium">Dashboard</h5>
       <hr className="my-4 border-t border-gray-300" />
-
-      <div className="flex flex-row">
-        <div className="flex flex-row">
-          <input 
+      <div class="flex flex-col sm:flex-row"> 
+    <div class="flex flex-row mb-2 sm:mb-0"> 
+        <input 
             type="text" 
             value={searchInput} 
             onChange={handleSearchInputChange} 
             placeholder="Search by reg no" 
-            className="border border-gray-300 px-2 py-1 mr-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <button onClick={handleGoTrack} className="bg-blue-950 text-white px-3 py-1 rounded-md focus:outline-none">Go track</button>
-        </div>
-        <div className="ml-auto"> 
-          <input 
+            class="border border-gray-300 px-2 py-1 mr-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button onClick={handleGoTrack} class="bg-blue-950 text-white px-3 py-1 rounded-md focus:outline-none">Go track</button>
+    </div>
+    <div class="ml-0 sm:ml-auto sm:mt-1">
+        <input 
             type="date" 
             value={startDate} 
             onChange={handleStartDateChange} 
-            className="border border-gray-300 px-2 py-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-         
-        </div>
-      </div>
+            class="border border-gray-300 px-2 py-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+    </div>
+</div>
+
 
       <div className="flex flex-col mt-4 md:flex-row">
         <div className="flex-1 p-4 mb-4 md:mb-0 md:mr-4" style={{ backgroundColor: "aliceblue" }}>
