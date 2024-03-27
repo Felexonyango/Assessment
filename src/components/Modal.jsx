9ec306c9afd66736bd3b0b86882b9fdeca4f98e8
Felexonyango <felexonyango02@gmail.com>
2024-03-27 17:04:50 +0300

    updated changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -57,7 +57,7 @@ const Modal = () => {
 
       <button
         type="button"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-950 hover:bg-blue-950 text-white font-bold py-2 px-4 rounded"
         onClick={handleModalOpen}
       >
         Open Modal
